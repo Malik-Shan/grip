698d7006dfefaa0ec106fa25dc9a514839f5bfcf
Malik-Shan <malikmuhammadarslanakbar133@gmail.com>
2023-06-11 01:26:41 +0500

    gg

--- src/Javascript/app.js
+++ src/Javascript/app.js
@@ -119,6 +119,71 @@ const searchModalOpen = document.querySelector('.fsb');
             }
         })
     })
+
+    const searchInput = document.querySelector('#search');
+
+    searchInput.addEventListener('input', async (e) => {
+        
+        let searchTimer
+        // console.log(searchInput);
+    
+        window.pagefind = await import ('/pagefind/pagefind.js');
+    
+        searchAndUpdate(e.target.value , window.pagefind);
+    })
+    
+    const searchAndUpdate = debounce( async (query, find) => {
+        if(query.length >= 1){
+            // search index
+        // console.log(query);
+        const search = await find.search(query);
+        // clears prev results
+        document.querySelector('#results').innerHTML = '';
+    
+        // Give results number
+        const resultnum = document.querySelector('.resultnum');
+        const num = search.results.length;
+        resultnum.textContent = `Results Found (${num})`;
+        // add new result
+        if(num >=1){
+            for( const result of search.results) {
+            const data = await result.data()
+            document.querySelector('#results').innerHTML += `
+            <div>
+                <a href="${data.url}">
+                <h3>${data.meta.title}</h3>
+                <img src="${data.meta.image}" style="width:30px;">
+                <p>${data.excerpt}</p>
+                </a>
+            </div>`
+        }
+        } else{
+            document.querySelector('#results').innerHTML = `
+            <span class="noresultsfound"> No Results Found </span>
+            `
+            ;
+        }
+        } else {
+            document.querySelector('.resultnum').textContent = '';
+            document.querySelector('#results').innerHTML = '';
+        }
+    } , 500)
+    // async function searchAndUpdate(query , find){
+        
+        
+    
+    // }
+    
+    function debounce(cb, delay = 1000){
+      let timeout
+      return (...arg) => {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          cb(...arg)
+        }, delay)
+      }
+    }
+    
 // window.addEventListener('keydown', (e)=>{
 //     const modal = document.querySelector('.searchModal');
 //     modal.hasAttribute('open');
